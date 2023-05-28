57cca2e3e3ba2a464b0a4431874e79de57ceca16
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2023-05-28 18:19:51 +0200

    task

--- Homework8/observer/Vacancy.java
+++ Homework8/observer/Vacancy.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Vacancy {
 
 
-    final String[] list_vacancy = {"Водитель", "Программист", "Уборщик", "Дизайнер", "Директор"};
+    final String[] list_vacancy = {"Водитель", "Программист", "Менеджер", "Дизайнер", "Директор"};
     Random rand = new Random();
     int index = rand.nextInt(list_vacancy.length);
     public String nameVacancy = list_vacancy[index];
